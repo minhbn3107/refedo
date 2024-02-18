66282b9fe607bf1b4f47c1bc192b894923ec7e84
bnminh <minhbn3107>
2024-02-18 16:17:09 +0700

    change mail sender, fix sending mail to another account

--- lib/mail.ts
+++ lib/mail.ts
@@ -1,36 +1,68 @@
-import { Resend } from "resend";
+// import { Resend } from "resend";
+import nodemailer from "nodemailer";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+// const resend = new Resend(process.env.RESEND_API_KEY);
+const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+    port: 465,
+    secure: true,
+    service: "gmail",
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.PASSWORD,
+    },
+});
 
 const domain = process.env.NEXT_PUBLIC_APP_URL;
 
 export const sendVerificationEmail = async (email: string, token: string) => {
     const confirmLink = `${domain}/auth/new-verification?token=${token}`;
 
-    await resend.emails.send({
+    await transporter.sendMail({
         from: "Next Auth <onboarding@resend.dev>",
         to: email,
         subject: "Confirm your email",
         html: `<p>Click <a href="${confirmLink}">here</a> to confirm email!</p>`,
     });
+
+    // await resend.emails.send({
+    //     from: "Next Auth <onboarding@resend.dev>",
+    //     to: email,
+    //     subject: "Confirm your email",
+    //     html: `<p>Click <a href="${confirmLink}">here</a> to confirm email!</p>`,
+    // });
 };
 
 export const sendPasswordResetEmail = async (email: string, token: string) => {
     const resetLink = `${domain}/auth/new-password?token=${token}`;
 
-    await resend.emails.send({
+    await transporter.sendMail({
         from: "Next Auth <onboarding@resend.dev>",
         to: email,
         subject: "Reset your password",
         html: `<p>Click <a href="${resetLink}">here</a> to reset password!</p>`,
     });
+
+    // await resend.emails.send({
+    //     from: "Next Auth <onboarding@resend.dev>",
+    //     to: email,
+    //     subject: "Reset your password",
+    //     html: `<p>Click <a href="${resetLink}">here</a> to reset password!</p>`,
+    // });
 };
 
 export const sendTwoFactorTokenEmail = async (email: string, token: string) => {
-    await resend.emails.send({
+    await transporter.sendMail({
         from: "Next Auth <onboarding@resend.dev>",
         to: email,
         subject: "2FA Code",
         html: `<p>Your 2FA code: ${token}</p>`,
     });
+
+    // await resend.emails.send({
+    //     from: "Next Auth <onboarding@resend.dev>",
+    //     to: email,
+    //     subject: "2FA Code",
+    //     html: `<p>Your 2FA code: ${token}</p>`,
+    // });
 };
